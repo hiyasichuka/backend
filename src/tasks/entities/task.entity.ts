68f639ea15e3c7b0163176fed8b2969d2a2b1fb0
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-05-17 14:25:40 +0900

    Update Entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,17 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from '../task-status.enum';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: TaskStatus;
+}
