8c712a933dccc003c3902f6412d7657da4288f22
hiyasichuka <takahashi.kgs@om.asahi-kasei.co.jp>
2022-05-18 10:06:34 +0900

    Add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
