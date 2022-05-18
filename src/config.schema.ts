3ba12b6d2d0b6646725fe558a6f720ffc7e8dd5b
hiyasichuka <takahashi.kgs@om.asahi-kasei.co.jp>
2022-05-18 14:28:28 +0900

    update PORT env

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
+  PORT: Joi.number().default(3000),
   NODE_ENV: Joi.string().valid('dev', 'prod').default('dev'),
   STAGE: Joi.string().required(),
   DB_HOST: Joi.string().required(),
