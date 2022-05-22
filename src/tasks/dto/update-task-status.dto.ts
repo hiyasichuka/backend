1a23cdb95d67682d91dbd71a33592aa0ef3caf3d
hiyasichuka <takahashi.kgs@om.asahi-kasei.co.jp>
2022-05-22 20:41:48 +0900

    Add api description

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -3,7 +3,11 @@ import { IsEnum } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
 
 export class UpdateTaskStatusDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Task status',
+    enum: ['OPEN', 'IN IN_PROGRESS', 'DONE'],
+    default: 'OPEN',
+  })
   @IsEnum(TaskStatus)
   status: TaskStatus;
 }
