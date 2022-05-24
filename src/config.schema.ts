863e3edd32409af0e92ef3a9ad9c16e91704e7be
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-05-24 19:52:34 +0900

    hotfix/NODE_ENV

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,7 +2,9 @@ import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().default(3000),
-  NODE_ENV: Joi.string().valid('development', 'production').default('dev'),
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'provision')
+    .default('development'),
   STAGE: Joi.string().required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.string().default(3306).required(),
