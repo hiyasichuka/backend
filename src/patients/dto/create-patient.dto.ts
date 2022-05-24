3ae8cead61edf02e402d599daeb1716d3ae9de35
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-05-24 20:46:33 +0900

    hotfix/dto

--- src/patients/dto/create-patient.dto.ts
+++ src/patients/dto/create-patient.dto.ts
@@ -1,7 +1,14 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreatePatientDto {
   first_name: string;
+
   last_name: string;
+
   birth_date: Date;
+
+  @IsNotEmpty()
   phone: number;
+  
   email: string;
 }
